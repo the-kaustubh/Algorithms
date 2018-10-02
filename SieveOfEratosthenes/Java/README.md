4d409e0a3441876b76c7ecd311b1eec4cb790052
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2018-10-02 11:20:47 +0530

    Add file via upload

---
+++ SieveOfEratosthenes/Java/README.md
@@ -0,0 +1,11 @@
+# Sieve of Eratosthenes
+## implemented in Java
+
+A great explanation of the algorithm is
+available on [Wikipeida](https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes "Sieve Of Eratosthenes")
+
+> To compile run the following on your command-line
+  `git clone https://github.com/the-kaustubh/Algorithms.git`
+  `cd SieveOfEratosthenes/Java`
+  `make`
+  `make run`
---
+++ SieveOfEratosthenes/Java/Sieve.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Sieve {
+  private int size, sieve[], primeCount;
+  Sieve(int n) {
+    this.size = n;
+    sieve = new int[n];
+    initializeWithZeros();
+    sievify();
+  }
+
+  void initializeWithZeros() {
+    for(int i = 2; i < this.size; i++) {
+      this.sieve[i] = 1;
+    }
+  }
+
+  void sievify() {
+    this.primeCount = this.size - 2; // Neglect 0 and 1
+
+    for(int i = 0; i < Math.sqrt(this.size); i++) {
+      if(this.sieve[i] == 1) { 
+        for(int j = i*i; j < this.size; j += i) {
+          if(this.sieve[j] == 1) {
+            this.primeCount--;
+          }
+          this.sieve[j] = 0;
+        }
+      }
+    }
+
+  }
+
+  int getPrimeCount() {
+    return this.primeCount;
+  }
+
+  void printPrimes() {
+    int counter = 1;
+    System.out.println("-------------------");
+    for(int i = 0; i < this.size; i++) {
+      if(this.sieve[i] == 1) {
+
+        System.out.print(i + " ");
+
+        counter++;
+
+        if(counter % 5 == 0) {
+          System.out.println();
+        }
+      }
+    }
+    System.out.println("\n-------------------");
+  }
+
+  public static void main(String a[])  {
+    int size;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the sieve: ");
+    size = sc.nextInt();
+
+    Sieve soe = new Sieve(size);
+    soe.printPrimes();
+    System.out.println("There are " + soe.getPrimeCount() + " prime numbers in the sieve.");
+  }
+}
